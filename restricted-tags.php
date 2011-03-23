11abde3ec56b1cb99e26bc3cd56042fc1e751e05
Brent Shepherd <thenbrent@gmail.com>
2011-03-23 14:32:11 +0700

    Restricting mmanage_categories to only admin user on activation. Removing custom tax column functions.

--- restricted-tags.php
+++ restricted-tags.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Restrict Tags & Add Columns for Custom Taxonomies
-Description: Allow site admins to restrict the tags available for other users on their site. Give custom taxonomies a column on the manage posts page. 
+Plugin Name: Restrict Tags
+Description: Allow site admins to restrict the tags & custom taxonomies available for other users on their site.
 Author: Jason Conroy, Brent Shepherd
 Version: 1.0
 */
@@ -53,6 +53,7 @@ function rt_custom_tag_metabox( $post ) {
 }
 
 
+
 /**
  * When a post is saved by a non-admin user, the tags are sent as an array because
  * they come from a hierarchal UI. This confuses the WordPress tag saving process,
@@ -74,65 +75,17 @@ add_action( 'init', 'rt_modify_tags_structure' );
 
 
 /**
- * Add a column to the manage posts page for each registered custom taxonomy. 
+ * On plugin activation, remove capabilities for editing/managing/deleting terms from non-admin roles
  **/
-function rt_add_columns( $columns ) {
+function ft_remove_term_caps(){
+	global $wp_roles;
 
-	$taxonomy_names = get_object_taxonomies( 'post' );
+	$roles = $wp_roles->get_names();
 
-	foreach ( $taxonomy_names as $taxonomy_name ) {
-
-		$taxonomy = get_taxonomy( $taxonomy_name );
-
-		if ( $taxonomy->_builtin )
+	foreach ( $roles as $role => $value ){
+		if( $role == 'administrator' )
 			continue;
-
-		$columns[ $taxonomy_name ] = $taxonomy->label;
-	}
-
-	return $columns;
-}
-add_filter( 'manage_posts_columns', 'rt_add_columns' ); //Filter out Post Columns with 2 custom columns
-
-
-/**
- * Add the terms assigned to a post for each registered custom taxonomy to the 
- * custom column on the manage posts page.
- **/
-function rt_column_contents( $column_name, $post_id ) {
-	global $wpdb;
-
-	$taxonomy_names = get_object_taxonomies( 'post' );
-
-	foreach ( $taxonomy_names as $taxonomy_name ) {
-		$taxonomy = get_taxonomy( $taxonomy_name );
-
-		if ( $taxonomy->_builtin || $column_name != $taxonomy_name )
-			continue;
-
-		$terms = get_the_terms( $post_id, $taxonomy_name ); //lang is the first custom taxonomy slug
-		if ( !empty( $terms ) ) {
-			$out = array();
-			foreach ( $terms as $term )
-				$termlist[] = "<a href='edit.php?$taxonomy_name=$term->slug'> " . esc_html( sanitize_term_field( 'name', $term->name, $term->term_id, $taxonomy_name, 'display' ) ) . "</a>";
-			echo join( ', ', $termlist );
-		} else {
-			printf( __( 'No %s.'), $taxonomy->label );
-		}
+		$wp_roles->remove_cap( $role , 'manage_categories' );
 	}
 }
-add_action( 'manage_posts_custom_column', 'rt_column_contents', 10, 2 );
-
-
-/**
- * Generate mock taxonomies for testing.
- **/
-function rt_mock_tax(){
-	$args = array( 'label' => 'Mocks' );
-	register_taxonomy( 'mock_tax', 'post', $args );
-
-	$args = array( 'label' => 'Faux', 'hierarchical' => true );
-	register_taxonomy( 'faux_tax', 'post', $args );
-}
-add_action( 'init', 'rt_mock_tax' );
-
+register_activation_hook( __FILE__, 'ft_remove_term_caps' );
